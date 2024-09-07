83fac930aaa1221802d3bde2e1c0847e5af7b39c
Ndiritu <ndiritumichael5@gmail.com>
2024-09-08 00:01:54 +0300

    implement fibonacci sequence without recursion

--- leetcode/fibonacci/Fibonnaci.go
+++ leetcode/fibonacci/Fibonnaci.go
@@ -25,3 +25,23 @@ func getFib(n int, fibMap map[int]int) int {
 	}
 	return result
 }
+func GetFibWithoutRecursion(n int) int {
+	var fibMap = make(map[int]int)
+
+	if n <= 0 {
+		return 0
+	}
+	if n == 1 || n == 2 {
+		return 1
+	}
+	fibMap[1] = 1
+	fibMap[2] = 1
+
+	for i := 3; i <= n; i++ {
+		fibMap[i] = fibMap[i-1] + fibMap[i-2]
+
+	}
+
+	return fibMap[n]
+
+}
